20f26750b74e9fb854a7da1c930ddb90490ff4d6
kelibst <kbooster17@gmail.com>
2023-08-28 08:56:22 +0000

    add the package browser image compressor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
+'use client'
 import ImageUpload from '@/components/ImageUpload'
 import Image from 'next/image'
 
 export default function Home() {
+  const handleImageUpload = (file: File) => {
+    // Implement your image upload logic here
+    console.log('Image uploaded:', file);
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-center font-bold">Image Compressor</h1>
-      <div className="z-10 max-w-2xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <ImageUpload />
+    <main className="block">
+      <h1 className="text-center font-bold my-6">Image Compressor</h1>
+      <div className="flex flex-col justify-center items-center min-h-screen">
+        <div className="">
+          <ImageUpload onImageUpload={handleImageUpload} />
+        </div>
       </div>
+
     </main>
   )
 }
--- src/components/ImageUpload.tsx
+++ src/components/ImageUpload.tsx
@@ -1,14 +1,50 @@
-import React from 'react'
+'use client'
+import React, { useCallback, useState } from 'react';
+import { MdCloudUpload } from 'react-icons/md';
+import Dropzone from 'react-dropzone';
 
-const ImageUpload = () => {
-    return (
-        <div>
-            <form>
-                <input type="file" name="image" id="image" />
-                <button type="submit">Upload Image</button>
-            </form>
-        </div>
-    )
+interface ImageUploadProps {
+    onImageUpload: (file: File) => void;
 }
 
-export default ImageUpload
+const ImageUpload: React.FC<ImageUploadProps> = ({ onImageUpload }) => {
+    const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+    const onDrop = useCallback((acceptedFiles: File[]) => {
+        const file = acceptedFiles[0];
+        setSelectedFile(file);
+        onImageUpload(file);
+    }, [onImageUpload]);
+
+    return (
+        <div className="image-upload">
+            {selectedFile && (
+                <img
+                    src={URL.createObjectURL(selectedFile)}
+                    alt="Uploaded"
+                    className="max-w-full max-h-48 mx-auto my-4"
+                />)
+            }
+            <Dropzone onDrop={onDrop} accept="image/*" multiple={false}>
+                {({ getRootProps, getInputProps }) => (
+                    <div
+                        className="dropzone border-2 border-dashed border-gray-300 p-6 cursor-pointer"
+                        {...getRootProps()}
+                    >
+                        <input {...getInputProps()} />
+                        {selectedFile ? (
+                            <div>Replace the current image  <MdCloudUpload className="text-4xl mb-2" /></div>
+                        ) : (
+                            <div className="flex flex-col items-center text-gray-500">
+                                <MdCloudUpload className="text-4xl mb-2" />
+                                <p>Drag & drop an image here or click to select</p>
+                            </div>
+                        )}
+                    </div>
+                )}
+            </Dropzone>
+        </div>
+    );
+};
+
+export default ImageUpload;
